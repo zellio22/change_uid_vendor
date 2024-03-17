1740a8c5e9b8abfb46c240938f19f4417ac7845f
zellio22 <49569121+zellio22@users.noreply.github.com>
2024-03-17 15:54:18 +0100

    Update setUSB.py

--- setUSB.py
+++ setUSB.py
@@ -4,5 +4,5 @@ board_config = env.BoardConfig()
 board_config.update("build.hwids", [
   ["0x27B8", "0x01ED"]
 ])
-board_config.update("build.usb_product", "LVDM_joystick")
+board_config.update("build.usb_product", "joystick")
 board_config.update("vendor", "Zellio")
